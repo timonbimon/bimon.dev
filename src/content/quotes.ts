870b94186ed4fb6fc30e239a58dca3364352011f
timonbimon <truban93@gmail.com>
2025-05-24 00:50:13 +0200

    Change Little Prince quote.

--- src/content/quotes.ts
+++ src/content/quotes.ts
@@ -67,7 +67,12 @@ export const quotes: Quote[] = [
     context: "Chronicle of a Death Foretold",
   },
   {
-    text: "All grown-ups were once children... but only few of them remember it.",
+    text: `À Léon Werth
+
+Je demande pardon aux enfants d’avoir dédié ce livre à une grande personne. J’ai une excuse sérieuse: cette grande personne est le meilleur ami que j’ai au monde. J’ai une autre excuse: cette grande personne peut tout comprendre, même les livres pour enfants. J’ai une troisième excuse: cette grande personne habite la France où elle a faim et froid. Elle a bien besoin d’être consolée. Si toutes ces excuses ne suffisent pas, je veux bien dédier ce livre à l’enfant qu’a été autrefois cette grande personne. Toutes les grandes personnes ont d’abord été des enfants. (Mais peu d’entre elles s’en souviennent.) Je corrige donc ma dédicace:
+
+À Léon Werth
+quand il était petit garçon.`,
     author: "Antoine de Saint-Exupéry",
     context: "The Little Prince",
   },
